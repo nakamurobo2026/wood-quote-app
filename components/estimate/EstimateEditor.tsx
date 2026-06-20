ad722823cded4b46c47d3e99fc6da5f09d9de036
nakamurobo2026 <nakamura0407@gmail.com>
2026-06-20 13:45:43 +0900

    Allow material master prop in estimate editor wrapper

--- components/estimate/EstimateEditor.tsx
+++ components/estimate/EstimateEditor.tsx
@@ -1,1 +1,9 @@
-export { EstimateEditor } from "../../EstimateEditor";
+import { EstimateEditor as BaseEstimateEditor } from "../../EstimateEditor";
+
+type EstimateEditorProps = Parameters<typeof BaseEstimateEditor>[0] & {
+  materials?: unknown[];
+};
+
+export function EstimateEditor({ materials: _materials, ...props }: EstimateEditorProps) {
+  return <BaseEstimateEditor {...props} />;
+}
